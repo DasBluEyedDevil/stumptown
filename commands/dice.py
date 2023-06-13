460cf95c94003b184a8e78c9a4b470ea5ebc59c6
Lem Canady <lem.canady@gmail.com>
2023-06-12 17:55:15 -0700

    fixing +roll

--- commands/dice.py
+++ commands/dice.py
@@ -59,9 +59,9 @@ class dice(MuxCommand):
             successes = "|y0|n"
 
         output.sort(key=lambda x: ANSIString((x)))
-        s_list = " ".join(output)
+        s_list = "".join(output)
 
         self.caller.location.msg_contents(
-            f"|w+ROLL>%cn |c{self.caller.name}|n rolls |w{dice_pool}|n dice -> {successes} successes ({s_list})")
+            f"|w+ROLL>|cn |c{self.caller.name}|n rolls |w{dice_pool}|n dice -> {successes} successes ({s_list})")
 
         # for every pair of 10s rolled, add 2 successes
