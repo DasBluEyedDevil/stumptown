0334f8d6d859a3baebc6fd95f0f04d689cf5881e
Lem Canady <lem.canady@gmail.com>
2023-06-17 09:49:16 -0700

    Sheet bug fix

--- commands/chargen.py
+++ commands/chargen.py
@@ -429,7 +429,7 @@ class cmdSheet(MuxCommand):
             output += social[i]
             output += "\n"
 
-        self.caller.msg(output.strip())
+        self.caller.msg(output.rstrip())
 
     def show_skills(self, target):
         """
