b8f9740a73291d6825840dfd817ba893e1e6f3e9
Lemuel Canady <lem.canady@gmail.com>
2023-07-09 17:36:47 -0700

    addded descer

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -22,7 +22,7 @@ from jobs.jobCmdSet import JobCmdSet
 from bbs.CmdSet import CmdSet as CmdBBS
 from .notes import cmdNotes, CmdNoteApprove, CmdNoteProve
 from .dice import dice
-
+from evennia.contrib.game_systems.multidescer import CmdMultiDesc 
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
     """
@@ -60,7 +60,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(dice())
         self.add(CmdRouse())
         self.add(CmdSlake())
-
+        self.add(CmdMultiDesc())
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
     """
